d9a1706144f2ca28596c11cd6916753834ad0376
TYCheng31 <117744027+TYCheng31@users.noreply.github.com>
2024-05-07 11:37:07 +0800

    Update script.js
    1.清理程式
    2.把警告改成存取傳送訊息跟時間的方式

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var savedmessages = [];
-var savedtimes = [];
+var savedmessages = [];//存傳送過的警告
+var savedtimes = [];//存傳送過警告的時間
 //時間
 function updateDateTime() {
   var currentDateTime = new Date();
@@ -17,18 +17,42 @@ function padNumber(number) {
 }     
 
 //門位 改門位圖示顏色
+var doorlock = 1;
 function fetchDoorData() {
   var xhttp = new XMLHttpRequest();
+  var doormessage;
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           var data = JSON.parse(this.responseText);
           var latestData = data[data.length - 1] || {};
           var timestampString = new Date(latestData.timestamp);
-          var gmtPlus8Time = timestampString.getFullYear() + '/' + (timestampString.getMonth() + 1) + '/' + timestampString.getDate() + ' ' + padNumber(timestampString.getHours()) + ':' + padNumber(timestampString.getMinutes()) + ':' + padNumber(timestampString.getSeconds());
           var indicatornumber = latestData.doornumber;
           var statusIndicator = document.getElementById('indicator' + indicatornumber);
-          
+          var doorlocate;
+
+          if(savedtimes[0] != null){
+            var gmtPlus8Time = savedtimes[0].getFullYear() + '/' + (savedtimes[0].getMonth() + 1) + '/' + savedtimes[0].getDate() + ' ' + 
+                                                    padNumber(savedtimes[0].getHours()) + ':' + padNumber(savedtimes[0].getMinutes()) + ':' + 
+                                                    padNumber(savedtimes[0].getSeconds());
+          }
+
+          //門位訊息的狀態
+          (indicatornumber <= 6) ?  doorlocate = '_前門' : doorlocate = '_後門';
+          if(indicatornumber > 6){
+            indicatornumber = indicatornumber / 2;
+          }
+          doormessage ='機櫃' + indicatornumber + doorlocate + '已開啟';
+
+          //找最新一筆門位資料選擇傳送LineNotify
+          if(latestData.doorstate == 1 && doorlock == 0){
+            sendLineNotify(timestampString , doormessage);
+            doorlock = 1;
+          }
+          else if(latestData.doorstate == 0){
+            doorlock = 0;
+          }
           //console.log(indicatornumber);
+
           document.getElementById('Updatetime').innerHTML = '上次異動時間: ' + gmtPlus8Time ;
           //門位被開啟顯示紅色
           if (latestData.doorstate === 1) {
@@ -43,12 +67,12 @@ function fetchDoorData() {
 }
 
 //溫溼度
-var temperaturelock = [1,1,1,1];
-var limit_temperature = [0,0,0,0];
+var temperaturelock = [1,1,1,1];//三個溫濕度警告的鎖
+var limit_temperature = [100,100,100,100];//初始限制溫度
 function fetchTH1Data() {
   var TemLocation = ["機櫃後溫度: ","冷氣出風口溫度: ","室內溫度: "];
   var HumLocation = ["機櫃後濕度: ","冷氣出風口濕度: ","室內溼度: "];
-  var temperaturemessage;
+
   //總共有三個溫溼度感測器
   for(let i=1;i<4;i++){
     var xhttp = new XMLHttpRequest();
@@ -56,14 +80,14 @@ function fetchTH1Data() {
         if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(this.responseText);
             var latestData = data[data.length - 1] || {};
-  
+            var timestampString = new Date(latestData.timestamp);
             document.getElementById('temperature' + i).innerHTML = TemLocation[i - 1] + latestData.temperature + 'C';
             document.getElementById('humidity' + i).innerHTML = HumLocation[i - 1] + latestData.humidity + '%';
             
             //溫度過高就傳送LineNotify
             if(latestData.temperature >= limit_temperature[i] && temperaturelock[i] == 0){
               (i == 1)?temperaturemessage = '機櫃後溫度過高':(i == 2)? temperaturemessage = '冷氣出風口溫度過高':temperaturemessage = '室內溫度過高';
-              sendLineNotify( temperaturemessage);
+              sendLineNotify(timestampString,temperaturemessage);
               temperaturelock[i] = 1;
             }
             else if(latestData.temperature < limit_temperature[i]){
@@ -77,45 +101,28 @@ function fetchTH1Data() {
   }
 }
 
-
 //警告
-var doorlock = 1;
 function fetchHISData() {
   var xhttp = new XMLHttpRequest();
-  var doormessage;
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var data = JSON.parse(this.responseText);
+        //取資料庫前十筆資料
         for(var i = 1; i < 11; i++){
           var latestData = data[data.length - i] || {};
           var timestampString = new Date(latestData.timestamp);
-          var gmtPlus8Time = timestampString.getFullYear() + '/' + (timestampString.getMonth() + 1) + '/' + timestampString.getDate() + ' ' + padNumber(timestampString.getHours()) + ':' + padNumber(timestampString.getMinutes()) + ':' + padNumber(timestampString.getSeconds());
-          var indicatornumber = latestData.doornumber;
-          var statusIndicator = document.getElementById('indicator' + indicatornumber);
           var today = new Date();
-          var doorlocate;
-          (indicatornumber <= 6) ?  doorlocate = '_前門' : doorlocate = '_後門';
-          if(indicatornumber > 6){
-            indicatornumber = indicatornumber / 2;
-          }
-
-          savedtimes.push(timestampString);
-          if(savedtimes.length > 10){
-            savedtimes.shift();
-          }
           //console.log(savedtimes);//第一項是第十筆最後一項才是最新一筆資料
-          
-          //歷史前十筆警告
-          if (timestampString.getDate() == today.getDate() || timestampString.getMonth() == today.getMonth() || timestampString.getFullYear() == today.getFullYear()) {
-            if(latestData.doorstate == 1){
-              document.getElementById('warning' + i).innerHTML = '事件' + i + ': ' + gmtPlus8Time + ': 機櫃' + indicatornumber + doorlocate + '開啟';
-            }
-            else if(latestData.doorstate == 0){
-              document.getElementById('warning' + i).innerHTML = '事件' + i + ': ' + gmtPlus8Time + ': 機櫃' + indicatornumber + doorlocate + '關閉';
-            }
+
+          //歷史警告
+          if(savedtimes[10 - i] != null){
+            var gmtPlus8Time = savedtimes[10 - i].getFullYear() + '/' + (savedtimes[10 - i].getMonth() + 1) + '/' + savedtimes[10 - i].getDate() + ' ' + 
+                                                    padNumber(savedtimes[10 - i].getHours()) + ':' + padNumber(savedtimes[10 - i].getMinutes()) + ':' + 
+                                                    padNumber(savedtimes[10 - i].getSeconds());
+            document.getElementById('warning' + i).innerHTML = gmtPlus8Time + savedmessages[10 - i];
           }
 
-          //今日前十筆警告
+          //今日警告
           if(timestampString.getDate() == today.getDate() && timestampString.getMonth() == today.getMonth() && timestampString.getFullYear() == today.getFullYear()){
             if(latestData.doorstate == 1){
               document.getElementById('todaywarning' + i).innerHTML = '事件' + i + ': ' + gmtPlus8Time + ': 機櫃' + indicatornumber + doorlocate + '開啟';
@@ -124,20 +131,6 @@ function fetchHISData() {
               document.getElementById('todaywarning' + i).innerHTML = '事件' + i + ': ' + gmtPlus8Time + ': 機櫃' + indicatornumber + doorlocate + '關閉';
             }
           }
-
-          if(i == 1){
-            doormessage ='機櫃' + indicatornumber + doorlocate + '已開啟';
-          } 
-        }
-        //找最新一筆門位資料選擇傳送LineNotify
-        var latestData = data[data.length - 1] || {};
-        var indicatornumber = latestData.doornumber;
-        if(latestData.doorstate == 1 && doorlock == 0){
-          sendLineNotify(doormessage);
-          doorlock = 1;
-        }
-        else if(latestData.doorstate == 0){
-          doorlock = 0;
         }
       }
   };
@@ -145,16 +138,23 @@ function fetchHISData() {
   xhttp.send();
 }
 
+
 //LineNotify
-function sendLineNotify(sendmessage) {
-  var message = sendmessage;//要傳送的訊息
+//0503只要有傳送訊息就會把警告訊息存進sendmessages，讓溫溼度警告跟門位警告資料合在一起
+//時間也是傳送訊息時會存起來至savedtimes
+function sendLineNotify(times , sendmessage) {
+  var message = sendmessage;
   var url = "/send-line-notify";
 
   savedmessages.push(message);
   if(savedmessages.length > 10){
     savedmessages.shift();
   }
-  console.log(savedmessages);
+  savedtimes.push(times);
+  if(savedtimes.length > 10){
+    savedtimes.shift();
+  }
+  //console.log(savedmessages);
   fetch(url, {
       method: 'POST',
       headers: {
@@ -193,9 +193,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
   //溫度確認按鈕
   submitbutton.addEventListener('click', function(){
-    limit_temperature[1] = limittemperature1.value;
-    limit_temperature[2] = limittemperature2.value;
-    limit_temperature[3] = limittemperature3.value;
+    limit_temperature = [100,100,100,100];
+
+    if(limittemperature1.value != ''){
+      limit_temperature[1] = limittemperature1.value;
+    }
+    if(limittemperature2.value != ''){
+      limit_temperature[2] = limittemperature2.value;
+    }
+    if(limittemperature3.value != ''){
+      limit_temperature[3] = limittemperature3.value;
+    }
     temperaturelock = [0,0,0,0];
     temmenu.classList.remove('active');
 
@@ -233,7 +241,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //網頁刷新資料頻率
 setInterval(updateDateTime, 1000); 
-updateDateTime();
 setInterval(fetchTH1Data, 1000);
 setInterval(fetchDoorData, 1000);
 setInterval(fetchHISData, 1000);
